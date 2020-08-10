8b8654a09a9f17c87c57640d1f4eb773f471793b
Piscinao <henhvj@gmail.com>
2020-08-10 13:14:35 -0300

    Test

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
-<?php
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
 
+  <p> Formulário para inscrição de competidores </p>
 
+  <form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome"></p>
+    <p>Sua idade: <input type="text" name="idade"></p>
+    <p><input type="submit" value="Enviar dados do competidor"></p>
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-
-print_f($categorias)
+  </form>
+    <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <script src="" async defer></script>
+  </body>
+</html>
---
+++ script.php
@@ -0,0 +1,38 @@
+<?php
+$categorias = [];
+$categorias = 'infantil';
+$categorias = 'adolescente';
+$categorias = 'adulto';
+$categorias = 'idoso';
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+var_dump($nome);
+var_dump($idade);
+
+return 0;
+if($idade >= 6 && $idade <= 12)
+{
+  for($i =0; $i <= count($categorias); $i++)
+  {
+    if($categorias[$i] == 'infantil')
+    echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
+  }
+}
+else if($idade >= 13 && $idade < 18)
+{
+  for($i = 0; $i <= count($categorias); $i++)
+  {
+    if($categorias[$i] == 'adolescente')
+    echo "O nadador " .$nome. " compete na categproa adolescente";
+  }
+}
+else
+{
+  for($i = 0; $i <= count($categorias); $i++)
+  {
+    if($categorias[$i] == 'adulto')
+     echo "O nadador " .$nome. " compete na categoria adulto";
+  }
+}
